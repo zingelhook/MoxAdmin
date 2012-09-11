d43f01aa60fb641a23d305b40cd9ca72e7d75c9f
Kevin Gaddy <kevin.gaddy@gmail.com>
2012-09-11 18:50:40 -0500

    mock fields

--- app/modules/mock.js
+++ app/modules/mock.js
@@ -3,13 +3,48 @@
 	Mock.Collection = Backbone.Collection.extend({});
 	Mock.Router = Backbone.Router.extend({});
 	var shared =  Suds.module("shared");
-	Mock.Model.Mock = Backbone.Model.extend({});
+	Mock.Model.Mock = Backbone.Model.extend({
+		getMockFields:function(callback){
+			var mdl = this;
+			var form_data = {
+				id: mdl.get('id')
+			};
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				url: base + "index.php/mock/GetMocksFields",
+				data: form_data,
+				success: function(msg) {
+					callback(msg);
+					//console.log(msg);
+					/*
+					var count = msg.UserMocks.length;
+					for (var i = 0; i < count; i++) {
+						var um = new Mock.Model.Mock({
+							name: msg.UserMocks[i].name,
+							id:msg.UserMocks[i].id,
+							max:msg.UserMocks[i].max,
+							min: msg.UserMocks[i].min
+						});
+						col.add(um);
+					}
+					*/
+				},
+				error: function(msg) {
+					console.log(msg);
+				}
+			});
+		}
+	});
+	Mock.Model.Field = Backbone.Model.extend({});
+	
 	Mock.Collection.Mocks = Backbone.Collection.extend({
 		model: Mock.Model.Mock,
 		loadData: function(callback, failcallback) {
 			var col = this;
 			var form_data = {
-				userid: 1
+				userid: Suds.app.currentUser.get('userId')
+				
 			};
 			$.ajax({
 				type: "GET",
@@ -36,6 +71,11 @@
 	});
 	
 	
+	Mock.Collection.MockFields = Backbone.Collection.extend({
+		model: Mock.Model.Field
+	});
+	
+	
 	Mock.Views.MocksTableRow  = Backbone.View.extend({
 	    tagName: "tr",
 		template: _.template("<td class='mock' id='<%=id%>'><%=name%></td>"),
@@ -52,6 +92,27 @@
 			info.render(function (el) {
 				$("#mock-info").html(el);
 			});
+		
+			var callback = function(msg){
+				var count = msg.MockFields.length;
+				var mockFields = new Mock.Collection.MockFields();
+				
+				for (var i = 0; i < count; i++) {
+					mockFields.add(msg.MockFields[i]);
+				}
+				
+	            var fieldTable = new Mock.Views.FieldTable({
+	                collection:mockFields
+	            });
+           
+	            fieldTable.render(function(el) {
+	                $("#mock-info").html(el);
+	            });
+			
+
+			}
+			mock.getMockFields(callback);
+			
 
 		},
 	    render: function () {
@@ -79,15 +140,58 @@
 	    }
 	});
 	
-	Mock.Views.MockInfo  = Backbone.View.extend({
-		template: _.template("<div class='info'><%=name%></div>"),
+	
+	
+	Mock.Views.FieldTableRow  = Backbone.View.extend({
+	    tagName: "tr",
+		template: _.template("<td class='field' id='<%=id%>'><%=name%></td><td><%=fieldoptions%></td><td><%=predefefinedSampleDataType%></td><td><%=sampleData%></td>"),
+
 	    initialize: function () {
 	        _.bindAll(this, "render");
 	    },
+
+	    render: function () {
+			var view=this;
+			var html = view.template(view.model.toJSON());
+			$(this.el).append(html);
+	        return this;
+	    }
+	});
+	
+	Mock.Views.FieldTable  = Backbone.View.extend({
+	    initialize: function () {
+	        _.bindAll(this, "render");
+	        this.collection.bind("all", this.render);
+	    },
+	    render: function () {
+			var view =this;
+	        var table = $("#mock-fields tbody");
+	        table.empty();
+	        this.collection.each(function (singelField) {
+	            var row = new Mock.Views.FieldTableRow({ model: singelField });
+	            table.append(row.render().el);
+	        });
+	        return this;
+	    }
+	});
+	
+	
+	Mock.Views.MockInfo  = Backbone.View.extend({
+		template: _.template("<div class='info'><ul class='unstyled'><li><h2>Name: <%=name%></h2></li><li>Min: <%=min%></li><li>Max: <%=max%></li></ul><h3>Fields</h3><table id='mock-fields' class='table table-bordered'><thead><tr><th>Name</th><th>Options</th><th>Type</th><th>Sample Data</th></thead><tbody><tbody></table></div>"),
+	    initialize: function () {
+	        _.bindAll(this, "render");
+	    },
+		_getMockFields:function(){
+			var userId = Suds.app.currentUser.get('userId');
+	
+		},
 	    render: function (done) {
 			var view=this;
+		
             view.el.innerHTML = view.template(view.model.toJSON());
             done(view.el);
+			
+			view._getMockFields();
 	        return this;
 	    }
 	});
