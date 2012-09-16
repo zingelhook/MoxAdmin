988ea28f3429446984f7b86e6b6c68c0f28c8393
Kevin Gaddy <kevin.gaddy@gmail.com>
2012-09-16 17:28:18 -0500

    added menu module

---
+++ app/modules/menu.js
@@ -0,0 +1,22 @@
+(function (Menu) {
+
+    Home.Model = Backbone.Model.extend({});
+    Home.Collection = Backbone.Collection.extend({});
+    Home.Router = Backbone.Router.extend({});
+
+    // This will fetch the tutorial template and render it.
+    Menu.Views.MainMenu = Backbone.View.extend({
+        template: "app/templates/Home.html",
+
+        render: function (done) {
+
+            var view = this;
+            // Fetch the template, render it to the View element and call done.
+            Suds.fetchTemplate(this.template, function (tmpl) {
+                view.el.innerHTML = tmpl({});
+                done(view.el);
+            });
+        }
+    });
+
+})(Suds.module("menu"));
