89d69d69411c947172c9e2e7bced4a484ef97eb3
Kevin Gaddy <kevin.gaddy@gmail.com>
2012-12-07 11:01:21 -0600

    config changes

---
+++ application/logs/log-2012-12-07.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-12-07 16:59:22 --> Severity: Warning  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: Access denied for user 'root'@'localhost' (using password: YES) C:\Users\kgaddy\Documents\GitHub\MoxAdmin\system\database\drivers\mysql\mysql_driver.php 88
+ERROR - 2012-12-07 16:59:22 --> Unable to connect to the database
