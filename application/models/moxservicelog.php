2335d705dece06b3e4a9cc30636f4a7479f172a4
Kevin Gaddy <kevin.gaddy@gmail.com>
2012-12-23 14:33:21 -0600

    case errors
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

--- application/models/moxservicelog.php
+++ application/models/moxservicelog.php
@@ -1,7 +1,7 @@
 <?php
 class moxservicelog extends CI_Model{
 	function GetAllServiceLogs(){
-			$sql = "select sl.logDate,sl.userAgent, st.name as MoxName,userId,CONCAT(firstName , ' ' , lastName) as name, userName from Service_DataTemplates st join users u on st.userId = u.id join service_log sl on st.id = sl.serviceId order by sl.logDate desc";
+			$sql = "select sl.logDate,sl.userAgent, st.name as MoxName,userId,CONCAT(firstName , ' ' , lastName) as name, userName from Service_DataTemplates st join Users u on st.userId = u.id join Service_Log sl on st.id = sl.serviceId order by sl.logDate desc";
 			return $this->db->query($sql)->result();
 		}	
 }
