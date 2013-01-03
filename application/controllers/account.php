c05c50349c8677218d221e8695aeca7337a3648e
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-01-02 18:25:04 -0600

    unused files
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

--- application/controllers/account.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class account extends CI_Controller {
-	public function index()
-	{
-			$data['main_content']='account';
-			$this->load->view('includes/template',$data);
-	}
-}
