dd95183832e8cc5167e547858b04207647189118
Kevin Gaddy <kevin.gaddy@gmail.com>
2013-01-21 19:33:00 -0600

    fixes sample data on field edit
    Signed-off-by: Kevin Gaddy <kevin.gaddy@gmail.com>

---
+++ application/logs/log-2013-01-15.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-01-15 01:48:33 --> Severity: Warning  --> mysql_pconnect(): Access denied for user 'root'@'localhost' (using password: YES) /Applications/MAMP/htdocs/MoxAdmin/system/database/drivers/mysql/mysql_driver.php 88
+ERROR - 2013-01-15 01:48:33 --> Unable to connect to the database
